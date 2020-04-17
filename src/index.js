b81909047f6d59e543f88e20e6770e9c6dcc610a
Sihao Huang <huangsihao@cs.dartmouth.edu>
2020-04-16 23:49:12 -0400

    deployed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Let the hunt begin!');
+// const $ = require('jquery')
+
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('Let\'s get the ball rolling!');
+
+let secElapsed = 0;
+
+setInterval(() => {
+  secElapsed += 1;
+  $('#subme').html(`You've been on this page for ${secElapsed} sec!`);
+}, 1000);
+
+// console.log('Let the hunt begin!');
