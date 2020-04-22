22808ff1cb5e441fc9f61cb2c3bfecdcaff40637
Sihao Huang <huangsihao@cs.dartmouth.edu>
2020-04-21 20:55:53 -0400

    startpack update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-// const $ = require('jquery')
-
 import $ from 'jquery';
 import './style.scss';
 
-$('#main').html('Let\'s get the ball rolling!');
+$('#main').html('Let\'s get started!');
 
 let secElapsed = 0;
 
@@ -11,5 +9,3 @@ setInterval(() => {
   secElapsed += 1;
   $('#subme').html(`You've been on this page for ${secElapsed} sec!`);
 }, 1000);
-
-// console.log('Let the hunt begin!');
