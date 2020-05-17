990a785e0d73b9492356bf7f2da70c312fc23778
Sihao Huang <huangsihao@cs.dartmouth.edu>
2020-05-17 07:40:30 -0400

    router support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
 import $ from 'jquery';
 import './style.scss';
 
@@ -9,3 +15,48 @@ setInterval(() => {
   secElapsed += 1;
   $('#subme').html(`You've been on this page for ${secElapsed} sec!`);
 }, 1000);
+
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        {/* <li><NavLink to="/">Home</NavLink></li> */}
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+        <li><NavLink to="/netherland">netherland</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          {/* without switch, all components are returned if the path matches */}
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));
--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,12 @@ const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { 
 
 module.exports = {
   mode: env,
+  output: { publicPath: '/' },
   entry: ['./src'], // this is where our app lives
   devtool: 'source-map', // this enables debugging with source in chrome devtools
   devServer: {
     hot: true,
+	historyApiFallback: true,
   },
   module: {
     rules: [
@@ -72,6 +74,10 @@ module.exports = {
     new HtmlWebpackPlugin({
 	  template: './src/index.html',
 	  filename: './index.html',
+    }),
+	new HtmlWebpackPlugin({
+	  template: './src/index.html',
+	  filename: './200.html',
     }),
   ],
 };
